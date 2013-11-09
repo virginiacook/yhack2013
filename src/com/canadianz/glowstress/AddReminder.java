14ef68904275248d6d4e8baf25695937454e3c38
virginiacook <virg.cook@earthlink.net>
2013-11-09 11:53:37 -0500

    added libraries for google android

--- src/com/canadianz/glowstress/AddReminder.java
+++ src/com/canadianz/glowstress/AddReminder.java
@@ -10,6 +10,8 @@ public class AddReminder extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_add_reminder);
+		
+		
 	}
 
 	@Override
